5fe36c295c36ae0bfb6a59ff370fa329bf1a5fe9
karanShaw000 <shawkaran2002@gmail.com>
2025-05-03 14:55:27 +0530

    add backend url

--- client/src/lib/network.ts
+++ client/src/lib/network.ts
@@ -1,9 +1,9 @@
 const getBaseUrl = (): string => {
-  // if (import.meta.env.DEV) {
-  //   return 'http://localhost:5000/api';
-  // }
-
+  if (import.meta.env.DEV) {
     return 'http://localhost:5000/api';
+  }
+
+    return 'notes-app.azurewebsites.net';
 }
 
 export default getBaseUrl
